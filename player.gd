a756feac8f2a0fcf378dc6277b5a395f368fb47b
Shatur95 <genaloner@gmail.com>
2021-05-26 00:23:53 +0300

    Implement jumping

--- player.gd
+++ player.gd
@@ -1,24 +1,19 @@
 extends KinematicBody3D
 
 
-const MAX_SPEED: float = 5
-const ACCELERATION: float = 500
-const GRAVITY: float = -10
+const MOVE_SPEED: int = 10
+const MOTION_INTERPOLATE_SPEED: int = 20
+const VELOCITY_INTERPOLATE_SPEED: int = 2
+const GRAVITY: int = 10
+const JUMP_IMPULSE: float = 4.0
 
 @onready var camera: PlayerCamera = $Camera
 
-var _horizontal_velocity: Vector3
-var _gravity: float = 0
-
-var _lines = 0
+var velocity: Vector3
+var on_floor: bool
 
 @onready var mesh_container: Node3D = $MeshContainer
 @onready var _mesh: MeshInstance3D = $MeshContainer/Mesh
-
-@onready var _ig = $debug/IG
-@onready var _ig_path = $debug/IG_path
-@onready var _ig_dir = $debug/IG_dir
-@onready var _ig_floor = $debug/IG_floor
 
 
 func set_color(color):
@@ -31,67 +26,22 @@ func set_color(color):
 	_mesh.set_surface_override_material(0, mat)
 
 
-func step_body(delta: float, input_direction: Vector3):
-	# Computes one motion step.
-
+ # Computes one motion step.
+func step_body(delta: float, input_direction: Vector3, is_jumping: bool):
 	_set_player_orientation(input_direction)
 
-	# Computes friction decelleration
-	_horizontal_velocity -= _horizontal_velocity.normalized() * ((_horizontal_velocity.length()/MAX_SPEED) * ACCELERATION * delta)
-	# Accelerate toward the new velocity, the combination of ACCELERATION and
-	# decelleration will converge to the `MAX_SPEED`
-	_horizontal_velocity += input_direction * (ACCELERATION * delta)
+	var motion: Vector3 = input_direction * MOVE_SPEED
+	var new_velocity: Vector3
+	if on_floor and velocity.length() < MOVE_SPEED:
+		new_velocity = velocity.lerp(motion, MOTION_INTERPOLATE_SPEED * delta)
+		if is_jumping:
+			new_velocity.y += JUMP_IMPULSE
+	else:
+		new_velocity = velocity.lerp(motion, VELOCITY_INTERPOLATE_SPEED * delta)
+		new_velocity.y -= GRAVITY * delta
 
-	# Compute GRAVITY velocity
-	_gravity += GRAVITY * delta
-
-	var on_floor_velocity = Vector3()
-	if is_on_floor():
-		on_floor_velocity = _horizontal_velocity.slide(get_floor_normal()).normalized()
-	var velocity = on_floor_velocity * _horizontal_velocity.length()
-	velocity[1] += _gravity
-
-	_ig.set_transform(get_global_transform())
-	_ig.clear()
-	_ig.begin(Mesh.PRIMITIVE_LINES)
-	_ig.add_vertex(Vector3())
-	_ig.add_vertex(velocity)
-	_ig.end()
-
-	_ig_floor.set_transform(get_global_transform())
-	_ig_floor.clear()
-	_ig_floor.begin(Mesh.PRIMITIVE_LINES)
-	_ig_floor.add_vertex(Vector3())
-	_ig_floor.add_vertex(get_floor_normal())
-	_ig_floor.end()
-
-	_ig_dir.set_transform(get_global_transform())
-	_ig_dir.clear()
-	_ig_dir.begin(Mesh.PRIMITIVE_LINES)
-	_ig_dir.add_vertex(Vector3())
-	_ig_dir.add_vertex(input_direction)
-	_ig_dir.end()
-
-	_ig_path.set_transform(Transform())
-	_lines += 1
-	if _lines % 100 >= 99:
-		_ig_path.clear()
-	_ig_path.begin(Mesh.PRIMITIVE_LINES)
-	_ig_path.add_vertex(get_global_transform().origin - Vector3(0, 0, 0))
-
-	move_and_slide(
-		velocity,
-		Vector3(0, 1, 0),
-		true,
-		4,
-		deg2rad(47),
-		true)
-
-	if is_on_floor():
-		_gravity = 0
-
-	_ig_path.add_vertex(get_global_transform().origin - Vector3(0, 0, 0))
-	_ig_path.end()
+	velocity = move_and_slide(new_velocity, Vector3.UP, true)
+	on_floor = is_on_floor() # Store in a variable to sync over network
 
 
 func _set_player_orientation(input_direction):
